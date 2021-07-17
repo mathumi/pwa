4b17011241f5bc6f36b5a7cbad15a1e1c67fc6d1
cb-mathu <mathumitha@chargebee.com>
2021-07-17 22:32:10 +0530

    sw offline files

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-console.log("SW file")
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('fox-store').then((cache) => cache.addAll([
+        '/',
+        '/index.html',
+        '/mathu.html',
+        '/style.css',
+        '/app.js',
+        '/img/icon-72x72.png',
+        '/img/icon-128x128.png',
+      ])),
+    );
+  });
